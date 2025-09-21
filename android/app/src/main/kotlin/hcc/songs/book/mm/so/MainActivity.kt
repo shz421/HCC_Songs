8efac4635a91a1866994a4c6fe676a2f29744215
shz <salai.hleihzue@gmail.com>
2025-09-21 11:39:50 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/hcc/songs/book/mm/so/MainActivity.kt
@@ -0,0 +1,5 @@
+package hcc.songs.book.mm.so
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:songs/Pages/Home.dart';
+
+
+void main() {
+  runApp(Appli());
+}
+
+class Appli extends StatelessWidget {
+  const Appli({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, theme: ThemeData());
+  }
+}
+
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    },
+    );
+  }
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/hcc.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Text(
+                    "Developed by BK",
+                    style: TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
